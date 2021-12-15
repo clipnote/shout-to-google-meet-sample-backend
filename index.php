13a9e346004dc5fc619653a3515e591f3efd5a53
Clip_note <yasuki.h10@gmail.com>
2021-12-16 08:43:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require 'vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *");
 
-$app_id = '';
-$app_key = '';
-$app_secret = '';
-$app_cluster = '';
+$app_id = getenv('APP_ID');
+$app_key = getenv('APP_KEY');
+$app_secret = getenv('APP_SECRET');
+$app_cluster = getenv('APP_CLUSTER');
 
 $result = ["meet_unique_id" => $_POST['meet_unique_id'], "message" => $_POST['message']];
 try {
